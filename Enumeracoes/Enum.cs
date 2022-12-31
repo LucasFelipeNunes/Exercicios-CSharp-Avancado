a0cfe0874b445e03d75dd54f6116f2abda9d741e
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-12-31 10:47:10 -0300

    Create Enum.cs

---
+++ Enumeracoes/Enum.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Enumeracoes {
+    class Enum {
+        public enum Genero { Filme, Serie, Documentario};
+        public class Filme{
+            public string Nome; 
+            public Genero TipoVideo;
+        }
+        static void Main() {
+            int cod = (int)Genero.Serie; 
+            Console.WriteLine(cod);
+        }
+    }
+}
